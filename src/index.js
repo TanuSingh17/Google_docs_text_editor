5926c85ffa9f1335dc46aa8c40f415b18f9381da
Tanu Singh <2002tanudixit@gmail.com>
2023-05-21 00:56:09 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+// import * as serviceWorker from './serviceWorker';
+import {createRoot} from "react-dom/client"
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
+
